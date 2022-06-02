a8c976b015507dd31ecaadb08b9fb15a605fcdfc
Taras Maliarchuk <tarasmalarcuk8@gmail.com>
2022-06-02 07:57:50 +0300

    added pausing a tender

--- src/components/Profile/ActivityPageComponents/ActivityCell.jsx
+++ src/components/Profile/ActivityPageComponents/ActivityCell.jsx
@@ -1,9 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import CustomButton from "../../CustomButton";
+import axios from "axios";
+import {tenders_request} from "../../../constants/rest_requests";
 
 const ActivityCell = ({field, index}) => {
     const [status, setStatus] = useState([])
     const [renderInfo, setRenderInfo] = useState([])
+    const [id, setId] = useState(0)
     useEffect(() => {
         const info = field.filter(it => !['status', 'id'].includes(it[0]))
         setRenderInfo(info)
@@ -15,8 +18,25 @@ const ActivityCell = ({field, index}) => {
         const status_ = field.filter(it => it[0] === 'status')[0][1]
         setStatus(status_)
     }, [])
-    const pause = async (status) => {
+    useEffect(() => {
+        const id_ = field.filter(it => it[0] === 'id')[0][1]
+        setId(id_)
+    }, [])
 
+    const pause = async () => {
+        setStatus(!status)
+        try{
+            await axios.post(tenders_request, {
+                id: id,
+                status: status,
+            },
+                {
+                    headers: {'Content-Type': "application/json"}
+                })
+        }
+        catch (e){
+            console.log(e)
+        }
     }
     return (
         <ul style={{'padding':'10px'}}
@@ -26,7 +46,10 @@ const ActivityCell = ({field, index}) => {
             <div className={'bth-group'}>
                 <CustomButton text={'delete'} style={{'backgroundColor': 'red'}}/>
                 {
-                    localStorage.role === 'customer' && (status === true ?  <CustomButton text={'pause'} /> :  <CustomButton text={'unpause'} />)
+                    localStorage.role === 'customer' && (status === true ?
+                        <CustomButton f={pause} text={'pause'} />
+                        :
+                        <CustomButton f={pause} text={'unpause'} />)
 
                 }
             </div>
